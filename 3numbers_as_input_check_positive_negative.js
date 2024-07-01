0daed87a929568a42ed7e6a676a9751b76f95f3f
MuhammadAmirDev24 <muhammadamirlqp@gmail.com>
2024-07-01 17:19:47 +0500

    Update 3numbers_as_input_check_positive_negative.js

--- 3numbers_as_input_check_positive_negative.js
+++ 3numbers_as_input_check_positive_negative.js
@@ -1,3 +1,5 @@
+//Take 3 numbers as input and print whether each number is positive or neg.
+
 let num1 = prompt("Enter First Number")
 let numm2 = prompt("Enter Second Number")
 let num3 = prompt("Enter Second Number")
